fbe7875da397ab846a6e25d66d0439c61d71f767
Shraddha1530 <99801417+Shraddha1530@users.noreply.github.com>
2022-03-04 17:23:24 +0530

    Add files via upload

---
+++ Exception/src/TryCatch.java
@@ -0,0 +1,22 @@
+
+public class TryCatch {
+
+	
+	    public static void main(String args[]) 
+	    {
+	        int[] array = new int[3];
+	        try 
+	        {
+	            array[7] = 3;
+	        }
+	        catch (ArrayIndexOutOfBoundsException e) 
+	        {
+	            System.out.println("Array index is out of bounds!"); 
+	        }
+	        finally 
+	        {
+	            System.out.println("The array is of size " + array.length);
+	        }
+	    }
+
+}
