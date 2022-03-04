e522f5b3e7538495d78b078cd3989e5761166101
Shraddha1530 <99801417+Shraddha1530@users.noreply.github.com>
2022-03-04 17:09:21 +0530

    Add files via upload

---
+++ Arithmetic/src/com/Calculator.java
@@ -0,0 +1,47 @@
+package com;
+import java.util.Scanner;
+	public class Calculator
+	{
+	 public static void main(String[] args)
+	 {
+		char operator;
+		Double number1, number2, result;
+		Scanner input=new Scanner(System.in);
+		System.out.println("Choose an operator:+,-,*,or /");
+		operator=input.next().charAt(0);
+		
+		System.out.println("Enter First Number: ");
+		number1=input.nextDouble();
+		
+		System.out.println("Enter Second Number");
+	    number2=input.nextDouble();
+	    
+	    switch(operator) {
+	    case '+':
+	    	result= number1+number2;
+	    	System.out.println(number1 +"+"+number2+"="+ result);
+	    	break;
+	    	
+	    case '-':
+	    	result= number1-number2;
+	    	System.out.println(number1 +"-"+number2+"="+ result);
+	    	break;
+	    	
+	    case '*':
+	    	result= number1*number2;
+	    	System.out.println(number1 +"*"+number2+"="+ result);
+	    	break;
+	    	
+	    case '/':
+	    	result= number1/number2;
+	    	System.out.println(number1 +"/"+number2+"="+ result);
+	    	break;
+	    	
+	    	default:
+	    		System.out.println("Invalid operator");
+	    		break;
+	    }
+		input.close();
+		}
+
+	}
